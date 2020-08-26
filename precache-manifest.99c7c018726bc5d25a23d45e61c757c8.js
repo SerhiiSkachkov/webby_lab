a59799206b8c37dd5bd5eb632e66786424a1015a
SerhiiSkachkov <sergeyskachkov01@gmail.com>
2020-08-26 16:58:53 +0300

    Updates

---
+++ precache-manifest.99c7c018726bc5d25a23d45e61c757c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7c64105e9501f6ab377298f9818cb7",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/index.html"
+  },
+  {
+    "revision": "27e085602ef9f582280e",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/static/css/main.d1f57e35.chunk.css"
+  },
+  {
+    "revision": "008d5a011654a043a601",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/static/js/2.1411621b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/static/js/2.1411621b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e085602ef9f582280e",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/static/js/main.059df308.chunk.js"
+  },
+  {
+    "revision": "e4995a0b66ad44ad76af",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/static/js/runtime-main.4f0e54c2.js"
+  },
+  {
+    "revision": "72de91db358fdf015511e4c3c8bc607c",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/static/media/Robotothin.72de91db.woff2"
+  },
+  {
+    "revision": "7faf23e11b3c2da59062fffc117e3a40",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/static/media/Robotothin.7faf23e1.ttf"
+  },
+  {
+    "revision": "a11d03f7bc9e68d84afdc5eea52ed121",
+    "url": "/SerhiiSkachkov.github.io/webby_lab/static/media/Robotothin.a11d03f7.woff"
+  }
+]);
